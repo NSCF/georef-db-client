838e804fbf02e7e3530bdf95ce69020666e402d9
Ian Engelbrecht <ianicus.za@gmail.com>
2022-10-05 01:40:00 +0200

    successfully fixed firebase build issues

--- src/firebaseAdmin.js
+++ src/firebaseAdmin.js
@@ -1,8 +1,8 @@
-import { getRecordGroup, countRecords, testFirebase, emptyCollection } from './firebaseAdminFuncs.js'
+//import { getRecordGroup, countRecords, testFirebase, emptyCollection } from './firebaseAdminFuncs.js'
 
 
 //emptyCollection('recordGroups').then(_ => console.log('all done'))
 
 //countRecords('recordGroups', 'completed', true).then(count => console.log(count, 'records match query'))
 
-getRecordGroup().then(groupKey => console.log(groupKey))
+//getRecordGroup().then(groupKey => console.log(groupKey))
